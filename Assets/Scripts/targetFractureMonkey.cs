fddc99a672d86e80f50a2253abf5b2a61bc3b331
sayronaught <sayro_naught@hotmail.com>
2021-10-07 18:16:28 +0200

    1st benchmark test scene

--- Assets/Scripts/targetFractureMonkey.cs
+++ Assets/Scripts/targetFractureMonkey.cs
@@ -7,10 +7,14 @@ public class targetFractureMonkey : MonoBehaviour
     public GameObject WholeGO;
     public GameObject FracturedGO;
 
+    public bool fractured = false;
+    public float timeLeft = 10f;
+
     private void OnTriggerEnter(Collider other)
     {
         WholeGO.SetActive(false);
         FracturedGO.SetActive(true);
+        fractured = true;
     }
 
     // Start is called before the first frame update
@@ -22,6 +26,9 @@ public class targetFractureMonkey : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!fractured) return;
+        timeLeft -= Time.deltaTime;
+        if (timeLeft > 0f) return;
+        Destroy(gameObject);
     }
 }
