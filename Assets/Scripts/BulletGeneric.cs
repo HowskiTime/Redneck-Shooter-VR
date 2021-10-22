2a1ee9c6f901746250fd3ce343ab7b8018d0f86a
sayronaught <sayro_naught@hotmail.com>
2021-10-22 11:42:41 +0200

    Decal direction better
    Need to sort that out before resizing and such

--- Assets/Scripts/BulletGeneric.cs
+++ Assets/Scripts/BulletGeneric.cs
@@ -6,11 +6,16 @@ public class BulletGeneric : MonoBehaviour
 {
     public GameController mainGC;
 
+
+
     private void OnCollisionEnter(Collision collision)
     {
         ContactPoint contact = collision.contacts[0];
-        GameObject decal = Instantiate(mainGC.gcResources.BulletHoles[0], contact.point, Quaternion.LookRotation(contact.normal));
+//        collision.contacts[0].normal;
+//        GameObject decal = Instantiate(mainGC.gcResources.BulletHoles[0], contact.point, Quaternion.LookRotation(contact.normal));
+        GameObject decal = Instantiate(mainGC.gcResources.BulletHoles[0], contact.point, Quaternion.LookRotation(collision.contacts[0].normal));
         decal.GetComponent<AudioSource>().Play();
+        Destroy(decal, 2f);
     }
 
     // Start is called before the first frame update
